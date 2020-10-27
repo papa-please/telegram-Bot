02586e4e6a3ed39148afb2fa4ad3b5c06609d855
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-10-27 09:55:56 +0100

    Update config.py

--- kick-inactive-group-members/config.py
+++ kick-inactive-group-members/config.py
@@ -7,9 +7,9 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = ""
-    APP_ID = ""
-    API_HASH = ""
+    BOT_TOKEN = "1372009343:AAE52f5J7IiY6koO2V58PaVptFKRx9IJZr8"
+    APP_ID = "1587067"
+    API_HASH = "34996e870761c2f772e86ee155079ff5"
 
 class Messages():
       HELP_MSG = [
