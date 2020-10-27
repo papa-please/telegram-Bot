70454afa476899a230a2987afbdf4a78d6f10056
Ice Queen <53878781+RomanGoddess@users.noreply.github.com>
2020-10-27 14:25:10 +0100

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -39,10 +39,10 @@ def map(pos):
             [InlineKeyboardButton(text = '-->', callback_data = "help+2")]
         ]
     elif(pos==len(tr.HELP_MSG)-1):
-        url = "https://github.com/viperadnan-git/telegram-bots/issues/new"
+        url = "https://t.me/joinchat/AAAAAE-44AkxSyqIMj1tdQ"
         button = [
-            [InlineKeyboardButton(text = 'Support Chat', url="https://t.me/ViperCommunity")],
-            [InlineKeyboardButton(text = 'Feature Request & Issues', url=url)],
+            [InlineKeyboardButton(text = 'Support Group', url="https://t.me/LeechZone")],
+            [InlineKeyboardButton(text = 'Premium Android Apps', url=url)],
             [InlineKeyboardButton(text = '<--', callback_data = f"help+{pos-1}")]
         ]
     else:
