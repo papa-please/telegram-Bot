92126e58b3df260e67cc0a7168c7fafa20388bce
Baby <69705110+BabyxD@users.noreply.github.com>
2020-12-14 21:23:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = "1487480282:AAG23ikbuVH-HaUcKe-mGVtPEAfcXsAtX18"
+    BOT_TOKEN = "1487480282AfcXsAtX18"
     APP_ID = "1061393"
     API_HASH = "dee44b60afe8fd325a96f5237e495d7a"
 
