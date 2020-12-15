898614a2f44ad1262782ac42814a41e68a7c47c4
The-Divya <76033254+The-Divya@users.noreply.github.com>
2020-12-15 12:46:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ class Config():
     APP_ID = os.environ.get("APP_ID", 6)
     API_HASH = os.environ.get("API_HASH", None)
   else:
-    BOT_TOKEN = "1459127199:AAECm17ytyLPkdbdMEwAbzUj20iDAEuJfv8"
+    BOT_TOKEN = "1487480282:AAEYnOICGLtZjmCUb9qTbre9ri5rmq0lT3Q"
     APP_ID = "1061393"
     API_HASH = "dee44b60afe8fd325a96f5237e495d7a"
 
